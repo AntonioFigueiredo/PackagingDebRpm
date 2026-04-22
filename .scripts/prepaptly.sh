40439fae80e1ca58788ded37928767780dd3884c
Antonio Figueiredo <antonio_figueiredo@web.de>
2026-04-22 11:51:13 +0200

    Add GPG key environment variables to packaging workflow and update prepaptly script for signing

--- .scripts/prepaptly.sh
+++ .scripts/prepaptly.sh
@@ -5,20 +5,25 @@ PKGDIR=pkgs
 mkdir -p ${PKGDIR}
 mv *-debpkg/*.deb ${PKGDIR}
 
-aptly repo create -distribution ${RELEASE} -component main ${PROJECT_NAME}
-aptly repo add ${PROJECT_NAME} ${PKGDIR}
-aptly repo show -with-packages ${PROJECT_NAME}
+aptly repo create -distribution "${RELEASE}" -component main "${PROJECT_NAME}"
+aptly repo add "${PROJECT_NAME}" "${PKGDIR}"
+aptly repo show -with-packages "${PROJECT_NAME}"
 
-ARCHITECTURES=$(aptly repo show -with-packages ${PROJECT_NAME} | \
+ARCHITECTURES=$(
+	aptly repo show -with-packages "${PROJECT_NAME}" | \
 		awk 'BEGIN {FS="_"} /^Packages:/ {x=NR} (x && NR>x) {print $3}' | \
 		egrep -v '^$' | sort -u | tr '\n' ','); \
 
-echo $ARCHITECTURES
+echo "$ARCHITECTURES"
 
 aptly publish repo \
-		-skip-signing \
-		${ARCHITECTURES:+ -architectures=${ARCHITECTURES}} \
-		${PROJECT_NAME}
+		-gpg-key="-kBD78A430515E1D36" \
+		${ARCHITECTURES:+ -architectures="${ARCHITECTURES}"} \
+		"${PROJECT_NAME}"
+
+if [ -n "${GPG_PUBLIC_KEY}" ]; then
+    echo "${GPG_PUBLIC_KEY}" > ~/.aptly/public/repo-public-key.asc
+fi
 
 find ~/.aptly
 
